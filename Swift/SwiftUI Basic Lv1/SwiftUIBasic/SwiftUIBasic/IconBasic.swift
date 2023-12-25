8986ae29d9b4316d5921fecd1121046bc682cb36
beomsu <kbs0439@gmail.com>
2023-12-25 22:09:00 +0900

    study Icon

---
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/IconBasic.swift
@@ -0,0 +1,18 @@
+//
+//  IconBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 김범수 on 12/25/23.
+//
+
+import SwiftUI
+
+struct IconBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconBasic()
+}
---
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/ImageBasic.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 김범수 on 12/25/23.
+//
+
+import SwiftUI
+
+struct ImageBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBasic()
+}
