15fbca42d5fdb9782490b4d1c8baa73812deec02
beomsu <kbs0439@gmail.com>
2023-12-29 20:44:16 +0900

    study ForEach

--- Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/ForEachLoopBasic.swift
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/ForEachLoopBasic.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ForEachLoopBasic: View {
+    //변수 설정
+    var data: [String] = ["Hi", "Hello", "Hey everyone"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            ForEach(0..<10){ index in
+                HStack(spacing: 20) {
+                    Circle()
+                        .frame(width: 20, height: 20, alignment: .center)
+                    
+                    Text("인덱스 번호는 \(index)")
+                } //:HSTACK
+            }
+            
+            Divider()
+            
+            ForEach(data, id: \.self){ item in
+                Text(item)
+            }
+            
+        } //:VSTACK
     }
 }
 
