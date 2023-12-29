3483a563366bee889516d54cec488dac9292298c
beomsu <kbs0439@gmail.com>
2023-12-29 20:43:36 +0900

    study ForEach

---
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/ForEachLoopBasic.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachLoopBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 김범수 on 12/29/23.
+//
+
+import SwiftUI
+
+struct ForEachLoopBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachLoopBasic()
+}
