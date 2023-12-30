35eefa57590df2dd08259e7e9e5341cede73e5fd
beomsu <kbs0439@gmail.com>
2023-12-30 22:55:10 +0900

    study navigation

--- Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/NavigationBasic.swift
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/NavigationBasic.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct NavigationBasic: View {
+    
+    @State var showSheet: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        NavigationView(content: {
+//            VStack {
+//                NavigationLink {
+//                    SecondNavigaionView()
+//                } label: {
+//                    Text("Second Navigation 이동")
+//                        .foregroundStyle(.white)
+//                        .font(.headline)
+//                        .padding()
+//                        .background(Color.blue)
+//                        .cornerRadius(10)
+//                }
+//
+//            }
+//            // 상단 페이지 제목
+//            .navigationTitle("페이지 제목")
+//            // 상단 페이지 제목 스타일
+//            // .automatic: 자동, inline - 상단에 작게, large : leading 쪽으로 크게
+//            .navigationBarTitleDisplayMode(.automatic)
+//            .toolbar(.hidden)
+//            
+////            .navigationbarleading
+//            
+//        })
+        
+        NavigationStack{
+            
+        }
     }
 }
 
--- Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/NavigationStackBasic.swift
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/NavigationStackBasic.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct NavigationStackBasic: View {
+    
+    @State var stack = NavigationPath()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $stack) {
+            NavigationLink("2번째 페이지 이동", value: 28)
+                .navigationDestination(for: Int.self) { value in
+                    VStack {
+                        
+                        NavigationLink("3번째 페이지 이동", value:  100)
+                        
+                        Text("나의 나이는 \(value)")
+                        Button {
+                            // Action
+//                            stack.removeLast() // 이전 페이지로 이동
+                            stack = .init() // 초기화면으로 바로 이동
+                        } label: {
+//                            Text("이전 페이지로 이동하기")
+                            Text("초기화면으로 이동하기")
+                                .bold()
+                        }
+                    } //: VSTACK
+                }
+        }
     }
 }
 
