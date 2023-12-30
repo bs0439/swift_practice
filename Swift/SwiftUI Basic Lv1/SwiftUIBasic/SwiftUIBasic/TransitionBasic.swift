c279b3b067cdc7be71f177f5534b0f61d1863437
beomsu <kbs0439@gmail.com>
2023-12-30 21:26:04 +0900

    study transition

---
+++ Swift/SwiftUI Basic Lv1/SwiftUIBasic/SwiftUIBasic/TransitionBasic.swift
@@ -0,0 +1,18 @@
+//
+//  TransitionBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 김범수 on 12/30/23.
+//
+
+import SwiftUI
+
+struct TransitionBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransitionBasic()
+}
